dfe1a2bd7484c62b3b3ea52e67c9262f3a6bab22
Jet Balagtas <jetbalagtas@gmail.com>
2019-03-14 20:34:24 -0400

    add content to recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'Just a test recipe',
+      'https://www.thepierhotellimerick.com/wp-content/uploads/2016/11/55735460-steak-wallpapers.jpg'
+    )
+  ];
 
   constructor() { }
 
